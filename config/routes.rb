863324fb91315437126032972e392c3a3b5496da
tabiojisann <takumiezumi@icloud.com>
2020-01-24 20:54:23 +0900

    created in update

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update, :destroy] do
-    resources :messages, only: [:index, :create, :destroy]
+    resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end 
 
 end
+
  
